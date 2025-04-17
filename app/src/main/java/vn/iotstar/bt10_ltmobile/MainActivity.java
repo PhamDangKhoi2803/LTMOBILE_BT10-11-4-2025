032a2b46e86325fdb444339444bbdf7a3e251ae2
DangKhoi <pdkhoi2004@gmail.com>
2025-04-17 20:08:22 +0700

    Web View Client

---
+++ app/src/main/java/vn/iotstar/bt10_ltmobile/MainActivity.java
@@ -0,0 +1,33 @@
+package vn.iotstar.bt10_ltmobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button buttonQuestion1 = findViewById(R.id.buttonQuestion1);
+        Button buttonQuestion2 = findViewById(R.id.buttonQuestion23);
+
+        buttonQuestion1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WebViewClientActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/vn/iotstar/bt10_ltmobile/WebViewClientActivity.java
@@ -0,0 +1,34 @@
+package vn.iotstar.bt10_ltmobile;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import vn.iotstar.bt10_ltmobile.databinding.ActivityWebviewclientBinding;
+
+public class WebViewClientActivity extends AppCompatActivity {
+    private ActivityWebviewclientBinding binding;
+    @SuppressLint({"SetJavaScriptEnabled", "WebViewApiAvailability"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityWebviewclientBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //getSupportActionBar().hide();
+        binding.webview.getSettings().setLoadWithOverviewMode(true);
+        binding.webview.getSettings().setUseWideViewPort(true);
+        binding.webview.getSettings().setJavaScriptEnabled(true);
+        binding.webview.setWebViewClient(new WebViewClient());
+        binding.webview.getSettings().setBuiltInZoomControls(true);
+        binding.webview.getSettings().setDomStorageEnabled(true);
+        binding.webview.getSettings().setDatabaseEnabled(true);
+        binding.webview.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        binding.webview.setWebChromeClient(new WebChromeClient());
+        binding.webview.loadUrl("https://www.youtube.com");
+    }
+}
